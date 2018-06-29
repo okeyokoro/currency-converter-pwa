739972907accb8429ffcf83fcaaf72e46538c7e0
cc_stoic <danielokeyokoro@gmail.com>
2018-06-28 17:42:40 -0700

    Auto-generated commit

--- sw.js
+++ sw.js
@@ -16,11 +16,10 @@ this.addEventListener('install', function(event) {
             (cache) => {
                 return cache.addAll([
                     '/index.html',
-                    '/main.js',
-                    '/polyfills.js',
-                    '/runtime.js',
-                    '/styles.js',
-                    '/vendor.js',
+                    '/main.5e197483e089933b15dc.js',
+                    '/polyfills.7a0e6866a34e280f48e7.js',
+                    '/runtime.a66f828dca56eeb90e02.js',
+                    '/styles.7621cbd6ab483dbe4bee.css',
                     '/manifest.json',
                     'https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxPKTU1Kg.ttf'
                 ]);
