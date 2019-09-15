db08d21eb77ca7dc396d4f9f5b73a6cfffdd1ee6
Daniel Okey-Okoro <daniel.okeyokoro@britecore.com>
2019-09-14 23:52:59 -0700

    docs: deployment instructions

--- README.md
+++ README.md
@@ -21,11 +21,16 @@ Run `npm install`
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
 
+## Deploy
+
+```bash
+ng build --prod --output-path currency-converter-pwa --base-href "https://okeyokoro.github.io/currency-converter-pwa"
+ngh --dir=currency-converter-pwa
+```
 
 ## Further help
 
---
+++ currency-converter-pwa/sw.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = "alc-cc-v5";
+
+this.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(
+            (cache) => {
+                return cache.addAll([
+                    '/index.html',
+                    '/main.js',
+                    '/polyfills.js',
+                    '/runtime.js',
+                    '/styles.js',
+                    '/vendor.js',
+                    '/manifest.json',
+                    'https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxPKTU1Kg.ttf'
+                ]);
+            }
+        )       
+    );
+})
+
+// delete stale caches
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.filter(function(cacheName) {
+            return cacheName.startsWith('alc-') &&
+                   cacheName != CACHE_NAME;
+          }).map(function(cacheName) {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+    );
+});
+
+// send requests over the network first and cache the response
+// if the network fails
+// try to serve from the cache
+self.addEventListener('fetch', event => {
+
+    event.respondWith(
+                
+        fetch(event.request)
+        .then(function(newResponse) {
+            const response = newResponse.clone();
+            console.log(`Storing network response`)
+
+            caches.open(CACHE_NAME).then(
+                cache => cache.put(event.request, response)
+                              .catch( err => console.log(`ERROR while attempting to cache: ${err}`))
+            )
+
+            return newResponse
+        })
+        .catch(function(error) {
+            console.log('Network failed. Now serving from cache', error)
+            return caches.match(event.request)
+        })
+
+    );
+});
+
+self.addEventListener('message', event => {
+    if (event.data.action == 'skipWaiting') {
+        console.log('message recieved');
+        self.skipWaiting();
+    }
+})
